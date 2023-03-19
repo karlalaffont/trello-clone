04af0f349c0f7a923b385440bea94fb1663c55f3
karlalaffont <karlalaffont@gmail.com>
2023-03-19 11:17:29 -0400

    commit19

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenService } from '@services/token.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private tokenService: TokenService,
+    private router: Router,
+  ){
+
+  }
+  canActivate():boolean
+ {
+  const token = this.tokenService.getToken();
+  if (!token){
+    this.router.navigate(['login']);
+    return false;
+  }
+    return true;
+  }
+
+}
---
+++ src/app/services/token.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { getCookie, setCookie, removeCookie } from 'typescript-cookie';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenService {
+  constructor() {}
+
+  savetoken(token: string) {
+    /*  localStorage.setItem('token', token); */
+    setCookie('token-trello', token, { expires: 365, path: '/' });
+  }
+
+  getToken() {
+    /* const token = localStorage.getItem('token'); */
+    const token = getCookie('token-trello');
+    return token;
+  }
+
+  removeToken() {
+    /*   localStorage.removeItem('token'); */
+    removeCookie('token-trello');
+  }
+}
