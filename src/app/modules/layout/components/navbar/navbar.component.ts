68725c365ceae30daf8fa835b3b4c76893b06b70
karlalaffont <karlalaffont@gmail.com>
2023-06-22 22:28:36 -0400

    commit

--- src/app/modules/layout/components/navbar/navbar.component.ts
+++ src/app/modules/layout/components/navbar/navbar.component.ts
@@ -6,8 +6,10 @@ import {
   faClose,
   faAngleDown
 } from '@fortawesome/free-solid-svg-icons';
+import { Colors, NAVBAR_BACKGROUNDS } from '@models/colors.model';
 
 import { AuthService } from '@services/auth.service';
+import { BoardsService } from '@services/boards.service';
 
 @Component({
   selector: 'app-navbar',
@@ -21,17 +23,34 @@ export class NavbarComponent {
 
   isOpenOverlayAvatar = false;
   isOpenOverlayBoards = false;
+  isOpenOverlayCreateBoard = false;
 
   user$ = this.authService.user$;
+  navBarBackgroundColor: Colors = 'sky';
+  navBarColors = NAVBAR_BACKGROUNDS;
 
   constructor(
     private authService: AuthService,
-    private router: Router
-  ) {}
+    private boardsService: BoardsService,
+    private router: Router,
+  ) {
+    this.boardsService.backgroundColor$.subscribe(color => {
+      this.navBarBackgroundColor = color;
+    });
+  }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  close(event: boolean) {
+    this.isOpenOverlayCreateBoard = event;
+  }
+
+  get colors() {
+    const classes = this.navBarColors[this.navBarBackgroundColor];
+    return classes ? classes : {};
   }
 
 }
